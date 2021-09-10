534862883db7bfe6889129079103b233e8ad15f5
Abolfazl Jelodar <jelodarabolfazl@gmail.com>
2021-09-10 11:33:27 +0430

    store app created

---
+++ store/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from store.views import baseView
+
+app_name = 'store'
+urlpatterns = [
+    path('', baseView, name='base'),
+]
---
+++ store/views.py
@@ -0,0 +1,20 @@
+from django.http import response
+from django.shortcuts import render
+
+from store.models import Product
+
+
+def popularProduct():
+    return Product.objects.all().order_by('-likes')[:5]
+
+
+def getProducts():
+    return Product.objects.all()
+
+
+def baseView(request):
+    popular_products = popularProduct()
+    all_products = getProducts()
+    return render(request, 'store/home.html',
+                  context={'popular_products': popular_products, 'all_products': all_products})
+    # return render(request, 'store/base.html')
