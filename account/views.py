e1b03ab795499e95b630c5806389f92183a4c776
Abolfazl Jelodar <jelodarabolfazl@gmail.com>
2021-09-10 12:52:36 +0430

    login func added but maybe not completed

--- account/views.py
+++ account/views.py
@@ -1,7 +1,24 @@
+from django.contrib.auth import authenticate
+from django.http import response
 from django.shortcuts import render
 
+# def registerValidation():
+#
+#
+# def register(request, username, password, firstname, lastname, email=None):
+#
 
-def LoginUser(request):
+def login(request,username, password):
+    if authenticate(username=username, password=password) or authenticate(phone=username, password=password):
+        return render(request, 'store/home.html')
+    else:
+        print('ascnkjasc')
+
+
+def rgisterLogin(request):
     if request.method == 'POST':
-        print('ok')
+        if request.POST['type'] == 'login':
+            login(request,request.POST['username'], request.POST['password'])
+        if request.POST['type'] == 'register':
+            print('not ok')
     return render(request, 'account/register-login.html')
