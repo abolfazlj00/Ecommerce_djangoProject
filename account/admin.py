d459cf1b42c7fe6ae929a1b8e9dd952465c1d3b4
Abolfazl Jelodar <jelodarabolfazl@gmail.com>
2021-09-10 11:39:17 +0430

    added user to admin

---
+++ account/admin.py
@@ -0,0 +1,25 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
+
+class CustomUserAdmin(UserAdmin):
+    # add_form = CustomUserCreationForm
+    # form = CustomUserChangeForm
+    model = CustomUser
+    list_display = ('phone', 'first_name', 'last_name', 'is_staff', 'is_active','is_superuser')
+    list_filter = ('phone', 'is_staff', 'is_active',)
+    fieldsets = (
+        (None, {'fields': ('phone', 'email', 'first_name', 'last_name')}),
+        ('Permissions', {'fields': ('is_staff', 'is_active')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('phone', 'password1', 'password2', 'is_staff', 'is_active')}
+         ),
+    )
+    search_fields = ('phone',)
+    ordering = ('phone',)
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
