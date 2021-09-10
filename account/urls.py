32db4f79bf37126dbbaf6729f3caf217fa862023
Abolfazl Jelodar <jelodarabolfazl@gmail.com>
2021-09-10 12:52:15 +0430

    some little changed

--- account/urls.py
+++ account/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 
-from account.views import LoginUser
+from account.views import rgisterLogin
 
 
 app_name = 'account'
 urlpatterns = [
-    path('sign-in/', LoginUser, name='login'),
+    path('sign-in/', rgisterLogin, name='login'),
 ]
