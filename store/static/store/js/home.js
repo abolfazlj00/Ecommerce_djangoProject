8a7f6bf63c53d59b3c6245fcad2f047e1000389f
Abolfazl Jelodar <jelodarabolfazl@gmail.com>
2021-09-16 03:22:59 +0430

    some changes in style of home page

---
+++ store/static/store/js/home.js
@@ -0,0 +1,45 @@
+// const full_star = document.createElement('I')
+// full_star.setAttribute('class', 'fas fa-star')
+// const full2 = full_star.cloneNode(true)
+// const full3 = full_star.cloneNode(true)
+// const full4 = full_star.cloneNode(true)
+// const full5 = full_star.cloneNode(true)
+//
+// // const half_star = document.createElement('I')
+// // half_star.setAttribute('class', 'fas fa-star-half-alt')
+// //
+// // const none_star = document.createElement('I')
+// // none_star.setAttribute('class', 'fas fa-star empty')
+// // const none2 = none_star.cloneNode(true)
+// // const none3 = none_star.cloneNode(true)
+// // const none4 = none_star.cloneNode(true)
+// // const none5 = none_star.cloneNode(true)
+//
+// const numbers = document.querySelectorAll('.pro-stars') // get all divs with this class (numbers = numbers of all products)
+//
+// for (var number of numbers) {
+//     var numberid = number.id
+//     let number_id = numberid.split(',')
+//     let customer_numbers = number_id[1]
+//     let likse = number_id[2]
+//     var percent = likse / customer_numbers
+//     number.appendChild(full_star)
+//     number.appendChild(full2)
+//     number.appendChild(full3)
+//     number.appendChild(full4)
+//     number.appendChild(full5)
+//     // if (percent === 1) {
+//     //     number.appendChild(full_star)
+//     //     number.appendChild(full2)
+//     //     number.appendChild(full3)
+//     //     number.appendChild(full4)
+//     //     number.appendChild(full5)
+//     // }
+//     // if (percent === 0) {
+//     //     number.appendChild(none_star)
+//     //     number.appendChild(none2)
+//     //     number.appendChild(none3)
+//     //     number.appendChild(none4)
+//     //     number.appendChild(none5)
+//     // }
+// }
