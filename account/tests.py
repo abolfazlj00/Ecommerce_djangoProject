39806259eda20e97b8120ac7296adbb3b553bcbc
Abolfazl Jelodar <jelodarabolfazl@gmail.com>
2021-09-16 03:26:53 +0430

    a simple test for register and login

--- account/tests.py
+++ account/tests.py
@@ -1,3 +1,36 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+User = get_user_model()
+
+
+class RegisterTest(TestCase):
+    def setUp(self) -> None:
+        self.data = {
+            "type": "register",
+            "username": 'karim',
+            "email": "",
+            "phone": '09123456777',
+            "password": "some123456"
+        }
+
+    def test_registerStatusCode(self):
+        response = self.client.post(reverse('account:login'), data=self.data)
+        self.assertEqual(response.status_code, 200)
+
+
+class LoginTest(TestCase):
+    def setUp(self):
+        self.credentials = {
+            'username': 'testuser',
+            'phone': '09145678238',
+            'password': 'secret'}
+        User.objects.create_user(**self.credentials)
+
+    def test_login(self):
+        # send login data
+        response = self.client.post(reverse('account:login'),
+                                    {'type': 'login', 'username': 'testuser', 'password': 'secret'})
+        # should be logged in now
+        self.assertEqual(response.status_code, 302)
