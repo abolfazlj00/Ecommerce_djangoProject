a65963b21fb4990b89d4ded93719315af7bfab3c
Abolfazl Jelodar <jelodarabolfazl@gmail.com>
2021-09-16 03:27:22 +0430

    model of Staff added to admin.register

--- store/admin.py
+++ store/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from store.models import Category, Product
+from store.models import Category, Product, Staff
 
 
 @admin.register(Category)
@@ -15,3 +15,8 @@ class ProductAdmin(admin.ModelAdmin):
     list_display = ['title', 'slug', 'price', 'stock', 'in_stock']
     list_filter = ['in_stock']
     prepopulated_fields = {'slug': ('title',)}
+
+
+@admin.register(Staff)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ['user']
