e9f0264840c6656c04a2ecade303684a84a5256b
Abolfazl Jelodar <jelodarabolfazl@gmail.com>
2021-09-16 03:26:23 +0430

    added an url for profile panel

--- account/urls.py
+++ account/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from account.views import rgisterLogin, logoutUser
+from account.views import rgisterLogin, logoutUser, profile
 
 app_name = 'account'
 urlpatterns = [
     path('sign-in/', rgisterLogin, name='login'),
     path('logout/', logoutUser, name='logout'),
+    path('profile/', profile, name='profile'),
 ]
