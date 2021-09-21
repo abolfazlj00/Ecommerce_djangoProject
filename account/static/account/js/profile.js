3da83dfc9bacfe40b2a173268fab3b5dc015948d
Abolfazl Jelodar <jelodarabolfazl@gmail.com>
2021-09-21 13:37:31 +0430

    func for change pass

--- account/static/account/js/profile.js
+++ account/static/account/js/profile.js
@@ -1,3 +1,4 @@
+// for show of page
 let getSiblings = function (e) {
     // for collecting siblings
     let siblings = [];
@@ -38,6 +39,37 @@ tabBtn.forEach(function (icon) {
             icon.classList.remove("clicked")
         })
     })
-
 })
 
+
+// change password without reloading
+const pass1 = document.getElementById('password').value
+const pass2 = document.getElementById('confirm_password').value
+const changePassForm = document.getElementById('change_pass')
+changePassForm.addEventListener('submit', async function (e) {
+    console.log(pass2, pass1)
+    e.preventDefault() // avoid to execute the actual submit of the form.
+    const changePassFormData = new FormData(changePassForm)
+    const url = changePassForm.getAttribute('action')
+    const formDataSerialized = Object.fromEntries(changePassFormData)
+    const csrf_token = changePassForm.getElementsByTagName("input")[0].value
+    if (pass1 === pass2) {
+        try {
+            const response = await fetch(url, {
+                method: 'POST',
+                body: JSON.stringify(formDataSerialized),
+                headers: {
+                    'Content-Type': 'application/json',
+                    'X-CSRFToken': csrf_token
+                }
+            })
+            const json = await response.json()
+            console.log(json)
+        } catch (e) {
+            console.log(e)
+            alert('something wrong, try again')
+        }
+    } else {
+        alert('The password and its confirm do not match')
+    }
+})
