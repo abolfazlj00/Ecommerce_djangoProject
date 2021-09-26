0fed3769685d262e01f28aba85212e5e8cce2fa0
Abolfazl Jelodar <jelodarabolfazl@gmail.com>
2021-09-26 05:10:24 +0330

    url for orders

--- order/urls.py
+++ order/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
 
-urlpatterns = [
+from order.views import updateItem, cart
 
+app_name = 'order'
+urlpatterns = [
+    path('cart/', cart, name='cart'),
+    path('update-item/', updateItem, name='update-item'),
 ]
