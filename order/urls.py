07fe5bb76fc8af4ccada7a1dda968ae0d734f721
Abolfazl Jelodar <jelodarabolfazl@gmail.com>
2021-09-28 04:20:45 +0330

    url for order history

--- order/urls.py
+++ order/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from order.views import updateItem, cart, checkout
+from order.views import updateItem, cart, checkout, orderHistory
 
 app_name = 'order'
 urlpatterns = [
     path('cart/', cart, name='cart'),
     path('update-item/', updateItem, name='update-item'),
     path('checkout/', checkout, name='checkout'),
+    path('order-history/', orderHistory, name='order-history'),
 ]
