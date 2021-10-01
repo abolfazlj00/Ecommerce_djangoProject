c61f1a4b9fe3721e3ab45f9a890ffd36d4e68718
Abolfazl Jelodar <jelodarabolfazl@gmail.com>
2021-10-01 09:49:13 +0330

    url for categories

--- store/urls.py
+++ store/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from store.views import baseView
+from store.views import baseView, deepCategoryView
 
 app_name = 'store'
 urlpatterns = [
     path('', baseView, name='home'),
+    path('category/all/<int:cat_id>', deepCategoryView, name='deep_category'),
 ]
