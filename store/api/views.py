05e4233e581764dbdd8eef75c07bc6265adabbca
Abolfazl Jelodar <jelodarabolfazl@gmail.com>
2021-10-01 09:49:29 +0330

    view for api of category

--- store/api/views.py
+++ store/api/views.py
@@ -1,8 +1,8 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from store.api.serializers import ProductSerializer
-from store.models import Product
+from store.api.serializers import ProductSerializer, CategorySerializer
+from store.models import Product, Category
 
 
 class ProductList(APIView):
@@ -13,4 +13,14 @@ class ProductList(APIView):
     def get(self, request):
         products = self.get_products()
         serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
+
+
+class CategoryList(APIView):
+    def getCategory(self):
+        return Category.objects.all()
+
+    def get(self, request):
+        categories = self.getCategory()
+        serializer = CategorySerializer(categories, many=True)
         return Response(serializer.data)
