b26d2b514ce3995a304cc7e5195cfdbe71c79f9b
Abolfazl Jelodar <jelodarabolfazl@gmail.com>
2021-10-01 09:49:06 +0330

    url for categories

--- store/api/urls.py
+++ store/api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from account.api.views import registrationView, changePassword
-from store.api.views import ProductList
+from store.api.views import ProductList, CategoryList
 
 app_name = 'api_store'
 urlpatterns = [
-    path('products/', ProductList.as_view())
+    path('products/', ProductList.as_view()),
+    path('categories/', CategoryList.as_view()),
 ]
