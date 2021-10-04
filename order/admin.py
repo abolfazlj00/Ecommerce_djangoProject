498eeb08d5955690459665044300687c570af0ec
Abolfazl Jelodar <jelodarabolfazl@gmail.com>
2021-10-04 08:23:50 +0330

    add orderItem inline for orders

--- order/admin.py
+++ order/admin.py
@@ -3,6 +3,24 @@ from django.contrib import admin
 # Register your models here.
 from order.models import Order, OrderItem, ShippingAddress
 
-admin.site.register(Order)
-admin.site.register(OrderItem)
-admin.site.register(ShippingAddress)
+
+class OrderItemTabularInline(admin.TabularInline):
+    model = OrderItem
+
+
+class OrderItemAdmin(admin.ModelAdmin):
+    model = OrderItem
+
+
+class OrderAdmin(admin.ModelAdmin):
+    inlines = [OrderItemTabularInline]
+    model = Order
+
+
+class ShippingAddressAdmin(admin.ModelAdmin):
+    model = ShippingAddress
+
+
+admin.site.register(OrderItem, OrderItemAdmin)
+admin.site.register(Order, OrderAdmin)
+admin.site.register(ShippingAddress, ShippingAddressAdmin)
