0c0020a61fc0b24f5ecf14308fe9e934e388df96
Abolfazl Jelodar <jelodarabolfazl@gmail.com>
2021-10-07 22:25:38 +0330

    add a view function for add a new address

--- customer/views.py
+++ customer/views.py
@@ -1,3 +1,21 @@
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+
+from customer.models import Customer
+from order.models import ShippingAddress
+
+
+@login_required
+def address(request):
+    if request.method == 'POST':
+        customer = request.user
+        province = request.POST['province']
+        city = request.POST['city']
+        user_address = request.POST['address']
+        postal_code = request.POST['postal_code']
+        new_address = ShippingAddress(customer=customer, province=province, city=city, address=user_address, postal_code=postal_code)
+        new_address.save()
+        return HttpResponse('True')
