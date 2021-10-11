6768b88abc74f564f80cf541faf54f619680f2ad
Abolfazl Jelodar <jelodarabolfazl@gmail.com>
2021-10-11 09:31:58 +0330

    function for showing customer's addresses.

--- customer/views.py
+++ customer/views.py
@@ -1,5 +1,7 @@
+import json
+
 from django.contrib.auth.decorators import login_required
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
 
 # Create your views here.
@@ -11,11 +13,13 @@ from order.models import ShippingAddress
 @login_required
 def address(request):
     if request.method == 'POST':
-        customer = request.user
-        province = request.POST['province']
-        city = request.POST['city']
-        user_address = request.POST['address']
-        postal_code = request.POST['postal_code']
-        new_address = ShippingAddress(customer=customer, province=province, city=city, address=user_address, postal_code=postal_code)
+        form = json.loads(request.body)
+        customer = Customer.objects.get(user=request.user)
+        province = form['province']
+        city = form['city']
+        user_address = form['address']
+        postal_code = form['postal_code']
+        new_address = ShippingAddress(customer=customer, province=province, city=city, address=user_address,
+                                      postal_code=postal_code)
         new_address.save()
-        return HttpResponse('True')
+        return JsonResponse({'response': 'True', 'id': f'address_{new_address.id}'})
