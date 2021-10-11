4ad6dea73b3d32330a6fa32c5ee60564b9fc9681
Abolfazl Jelodar <jelodarabolfazl@gmail.com>
2021-10-11 09:25:48 +0330

    add field of discount to model of customer. delete model of like.

--- customer/models.py
+++ customer/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth import get_user_model
 from django.db import models
 
 from account.models import CustomUser
-from store.models import Product
+from store.models import Discount
 
 User = get_user_model()
 
@@ -13,6 +13,7 @@ User = get_user_model()
 class Customer(models.Model):
     user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
     customer_id = models.PositiveSmallIntegerField(blank=True, null=True)
+    discount_code = models.ForeignKey(Discount, on_delete=models.CASCADE, null=True, blank=True)
 
     def save(self, *args, **kwargs):
         self.customer_id = int(f'{datetime.now().year}{datetime.now().month}{datetime.now().day}{randint(1000, 9999)}')
@@ -21,10 +22,3 @@ class Customer(models.Model):
     def __str__(self):
         return f'{self.user}'
 
-
-class Like(models.Model):
-    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return f'{self.product} has been liked by {self.customer}'
