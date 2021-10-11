fd35a136e05163208497582411533bdcd383ddca
Abolfazl Jelodar <jelodarabolfazl@gmail.com>
2021-10-11 09:27:31 +0330

    changes for light of page

--- store/static/store/js/base.js
+++ store/static/store/js/base.js
@@ -24,7 +24,9 @@ themeToggler.onclick = () => {
     themeToggler.classList.toggle('fa-sun');
     if (themeToggler.classList.contains('fa-sun')) {
         document.querySelector('body').classList.add('active');
+        localStorage.setItem('light', '0')
     } else {
         document.querySelector('body').classList.remove('active');
+        localStorage.setItem('light', '1')
     }
 }
